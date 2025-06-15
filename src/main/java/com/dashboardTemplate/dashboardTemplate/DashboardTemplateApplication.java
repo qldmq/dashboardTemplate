1b0d584fbb26387612e654e5332460bcaf9cd163
qldmq <ksh13345@naver.com>
2025-06-15 22:23:45 +0900

    프로젝트 초기화

---
+++ src/main/java/com/dashboardTemplate/dashboardTemplate/DashboardTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.dashboardTemplate.dashboardTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dashboardTemplate/dashboardTemplate/DashboardTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboardTemplate.dashboardTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
