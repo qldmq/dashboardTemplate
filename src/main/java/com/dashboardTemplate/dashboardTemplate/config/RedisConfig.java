f4f361c878c3ad662c1898f9ddc538ce61e88382
qldmq <ksh13345@naver.com>
2025-06-25 21:22:17 +0900

    Feat | login시 Redis에 Token 저장 로직 구현

---
+++ src/main/java/com/dashboardTemplate/dashboardTemplate/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.dashboardTemplate.dashboardTemplate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
