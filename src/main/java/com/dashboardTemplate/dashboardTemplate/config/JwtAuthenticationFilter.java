c58d3f506f97c6793d4a8908077cf7aec5d70b32
qldmq <ksh13345@naver.com>
2025-06-26 04:10:46 +0900

    swagger 권한 수정

--- src/main/java/com/dashboardTemplate/dashboardTemplate/config/JwtAuthenticationFilter.java
+++ src/main/java/com/dashboardTemplate/dashboardTemplate/config/JwtAuthenticationFilter.java
@@ -47,6 +47,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.startsWith("/swagger-ui") ||
+                path.startsWith("/v3/api-docs") ||
+                path.startsWith("/swagger-resources");
+    }
+
     private String resolveToken(HttpServletRequest request) {
         String bearer = request.getHeader("Authorization");
         if (bearer != null && bearer.startsWith("Bearer ")) {
