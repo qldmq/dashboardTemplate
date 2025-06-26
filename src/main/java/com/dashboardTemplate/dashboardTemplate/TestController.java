872f91e68575bd8adde3ca6d7bd817899a869ae5
qldmq <ksh13345@naver.com>
2025-06-27 00:14:55 +0900

    jenkins 자동배포 테스트

--- src/main/java/com/dashboardTemplate/dashboardTemplate/TestController.java
+++ src/main/java/com/dashboardTemplate/dashboardTemplate/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Operation(summary = "jenkins 자동배포 테스트2", description = "자동배포 테스트2")
+    @Operation(summary = "jenkins 자동배포 테스트3", description = "자동배포 테스트3")
     @GetMapping("/hello1")
     public String hello() {
         return "Hello, DashboardTemplate";
