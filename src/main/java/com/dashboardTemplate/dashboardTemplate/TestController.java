4e0a13c330c08b7cf229c3990f078fad1d65408c
qldmq <ksh13345@naver.com>
2025-06-26 23:32:07 +0900

    logback-classic 의존성 추가

--- src/main/java/com/dashboardTemplate/dashboardTemplate/TestController.java
+++ src/main/java/com/dashboardTemplate/dashboardTemplate/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Operation(summary = "jenkins 자동배포 테스트", description = "자동배포 테스트")
+    @Operation(summary = "jenkins 자동배포 테스트2", description = "자동배포 테스트2")
     @GetMapping("/hello1")
     public String hello() {
         return "Hello, DashboardTemplate";
