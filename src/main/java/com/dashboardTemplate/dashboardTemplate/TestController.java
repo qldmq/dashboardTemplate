47e3b4a4e3494a0c65f0b11e6398cdcc6139c0e1
qldmq <ksh13345@naver.com>
2025-06-26 22:42:52 +0900

    jenkins 자동배포 테스트

--- src/main/java/com/dashboardTemplate/dashboardTemplate/TestController.java
+++ src/main/java/com/dashboardTemplate/dashboardTemplate/TestController.java
@@ -1,15 +1,17 @@
 package com.dashboardTemplate.dashboardTemplate;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Operation(summary = "jenkins 자동배포 테스트", description = "자동배포 테스트")
     @GetMapping("/hello1")
     public String hello() {
         return "Hello, DashboardTemplate";
     }
 
-    //Jenkins 테스트
+    //Jenkins 자동 배포 
 }
