1915c5f19e6d948e291582e6b3a2ccd56cd55edf
qldmq <ksh13345@naver.com>
2025-07-08 12:56:44 +0900

    Fix | swagger에 security 설정 추가

--- src/main/java/com/dashboardTemplate/dashboardTemplate/config/SwaggerConfig.java
+++ src/main/java/com/dashboardTemplate/dashboardTemplate/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.dashboardTemplate.dashboardTemplate.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +12,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "Authorization";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .addServersItem(new Server().url("https://dashboardtemplate.duckdns.org"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
                 .info(new Info()
                         .title("API 문서 제목")
                         .description("API 문서 설명")
